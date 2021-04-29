c04fe44b5105d39dc1fb617d92eb35875f18fe00
Zachary Gaines <zacharygaines28@gmail.com>
2021-04-28 18:22:12 -0700

    app js and pokemon list pokemon item done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,93 @@
 import { Component } from 'react';
-import logo from './react-logo.svg';
+import request from 'superagent'
 import './App.css';
 import React from 'react';
+import Header from '../src/Header/Header.js';
+import PokemonList from '../src/PokemonList.js'
+
+const POKEMON_API_URL = 'https://pokedex-alchemy.herokuapp.com/api/pokedex';
 
 class App extends Component {
+  state = {
+    pokemon: null;
+    loading: false,
+    search: '',
+    page: 1
+  }
+
+  componentDidMount() {
+    this.fetchPokemon();
+  }
+
+  async fetchPokemon() {
+    const { search, page } = this.state
+
+    this.setState({ loading: true });
+
+    try {
+      const response = await request
+        .get(POKEMON_API_URL)
+        .query({ pokemon: search })
+        .query({ page: page });
+
+      this.setState([pokemon: response.body])
+    }
+    catch (err) {
+      console.log(err)
+    }
+    finally {
+      this.setState({ loading: false })
+    }
+  }
+
+  handleSearch = ({ search }) => {
+    this.setState(
+      { search: search, page: 1 },
+      () => this.fetchPokemon()
+    );
+  }
+
+  handlePrevPage = () => {
+    this.setState(
+      { page: Math.max(this.state.page - 1, 1) },
+      () => this.fetchPokemon()
+    );
+  }
+
+  handleNextPage = () => {
+    this.setState(
+      { page: this.state.page + 1 },
+      () => this.fetchPokemon()
+    );
+  }
 
   render() {
+    const { pokemon, loading, page }
+
     return (
       <div className="App">
-  
-        My React App...
-        
-        <img src={logo} className="temp-images" alt="react logo" />
-        <img src="acl-logo.png" className="temp-images" alt="acl logo" />
-      
+
+        <Header />
+
+        <section className="search-options">
+          <Search onSearch={this.handleSearch} />
+          <Paging
+            page={page}
+            onPrev={this.handlePrevPage}
+            onNext={this.handleNextPage}
+          />
+        </section>
+
+        <main>
+          {pokemon && (pokemon.length
+            ? <PokemonList pokemon={pokemon} />
+            : <p>Sorry no pokemon...(</p>)
+          }
+
+          {loading && <img className="loading" src="loading.gif" alt="loading" />}
+
+        </main>
+
       </div>
     );
   }
---
+++ src/PokemonItem.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import './PokemonItem.css'
+import PokemonList from './PokemonList';
+
+export default class PokemonItem extends Component {
+  render() {
+    const { pokemon } = this.props;
+
+    return (
+      <li className="PokemonItem">
+        <h2>{pokemon.pokemon}</h2>
+        <p className="type-1">{pokemon.type_1}</p>
+        <p className="type-2">{pokemon.type_2}</p>
+
+        <img
+          src={pokemon.image}
+          alt={pokemon.pokemon}
+        />
+      </li>
+    );
+  }
+}
+export default PokemonItem;
